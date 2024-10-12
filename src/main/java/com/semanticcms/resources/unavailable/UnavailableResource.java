f643cc823b517a77d213b4be9c298a9670b381fa
AO Industries, Inc <support@aoindustries.com>
2024-10-12 00:22:25 -0500

    checkstyle-10.18.2 compatibility

--- src/main/java/com/semanticcms/resources/unavailable/UnavailableResource.java
+++ src/main/java/com/semanticcms/resources/unavailable/UnavailableResource.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-resources-unavailable - Unavailable sets of SemanticCMS resources.
- * Copyright (C) 2017, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2017, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -34,6 +34,9 @@ import java.io.IOException;
  */
 public class UnavailableResource extends Resource {
 
+  /**
+   * Creates a new {@link UnavailableResource}.
+   */
   public UnavailableResource(UnavailableResourceStore store, Path path) {
     super(store, path);
   }
